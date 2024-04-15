7457edef98b25d26607b89a84d2a03aed9f3d9d5
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-04-15 14:08:21 +0200

    modifying Greeter.java

--- src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
+++ src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
@@ -3,20 +3,33 @@ package se.salt.precourse.firstjavaapp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
 
 public class Greeter {
   private static String greet(String namePassedIn) {
-    return "Welcome to SALT, ";
+    return "Welcome to SALT, " + namePassedIn;
   }
+    private static String calcualte(String inputdate) {
+      LocalDate targetdate= LocalDate.parse(inputdate);
+      LocalDate currentdate=LocalDate.now();
+      long daysRemaining = ChronoUnit.DAYS.between(currentdate, targetdate);
+      return "Today it is " + daysRemaining  +" days left until the course starts";
+    }
 
   public static void main(String[] args) throws IOException {
     System.out.print("What is your name? ");
 
+    Scanner Sc = new Scanner(System.in);
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    String name = reader.readLine();
-
+    String name = Sc.nextLine();
+    System.out.print("When does that course start?");
+    String inputdate = Sc.nextLine();
     String greeting = greet(name);
-
+    String Date= calcualte(inputdate);
     System.out.println(greeting);
+    System.out.println(Date);
+
   }
 }
